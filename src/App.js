953835d808f626753d1c499bc4ec238f7dd95f4f
cooper.chen <>
2018-10-07 18:28:42 +0800

    component property

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from './components/layout/Sidebar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import NameForm from './components/prompt/NameForm';
+import PropViewer from './components/prompt/PropViewer';
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
                     <Switch>
                         <Route path='/' component={Home}  exact={true} />
                         <Route path='/prompt' component={NameForm} />
+                        <Route path='/props' component={PropViewer} />
                     </Switch>
                 </div>
             </div>
