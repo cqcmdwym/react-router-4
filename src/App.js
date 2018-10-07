34c78a11d4b2086febc32e8030c00ab347840642
cooper.chen <>
2018-10-07 17:36:31 +0800

    dynamic router

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/layout/Header';
+import Sidebar from './components/layout/Sidebar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+
+const App = () => {
+
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Sidebar />
+                <div className='rightContentContainer'>
+
+                    {/* <Route path='/' component={Home} exact/>
+                    <Route path='/foo' component={Home} /> */}
+                    <Switch>
+                        <Route path='/' component={Home}  exact={true} />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
+};
+
+
+export default App;
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+
+    return (
+        <div className={'leftNavContainer'}>
+            <Link to='/' className="navLink">Home</Link>
+        </div>
+    );
+};
+
+export default Sidebar;
